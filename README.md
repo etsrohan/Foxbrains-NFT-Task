8918afa190fb07ba972567ada27a8ec52ea077d0
nexgrohan <rohan.srivastava@nexg.in>
2022-03-13 01:47:25 +0530

    Initiated creating the NFT contract and have a game plan

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Foxbrains-NFT-Task
 Repository for the NFT Task from Foxbrains 2nd Round Interview
-![Task](Foxbrains%20-%20Interview%20Task.PNG)
+![Task](./images/Foxbrains%20-%20Interview%20Task.PNG)
---
+++ contracts/Foxbrains_NFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+// Requirements:
+//      1) Mint NFT with ERC721 and can mint max 5000 NFTs
+//      2) Create a whitelist user for NFT
+//          - Only smart contract creators can add whitelisted users
+//          - Whitelist users can mint up to 10 NFT and other users can mint 5 NFTs
+//      3) Function to burn NFTs
+//      4) Create an ERC20 token named foxbrains
+
+pragma solidity ^0.8.4;
+
+contract FoxbrainsNFT {
+    // VARIABLES
+    address admin;
+
+    // MODIFIERS
+
+    // EVENTS
+
+    // FUNCTIONS
+}
